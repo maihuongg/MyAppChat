4a8fc4d7b70597fbea81998a7fb5d3d9f14d50de
maihuongg <dangmaihuong1108@gmail.com>
2023-05-11 21:52:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myappchat/IntroActivity.java
@@ -0,0 +1,25 @@
+package com.example.myappchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_layout);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000); // chuyển trang sau 3 giây
+
+    }
+}
---
+++ app/src/main/java/com/example/myappchat/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myappchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        //will hide the title not the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //int flag, int mask
+        setContentView(R.layout.activity_main);
+        drawerLayout= findViewById(R.id.drawer_layout);
+//
+//// Open the navigation drawer
+        drawerLayout.openDrawer(GravityCompat.START);
+//
+//// Close the navigation drawer
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+    }
+}
