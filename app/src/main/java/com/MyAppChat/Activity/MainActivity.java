21a8ad5185db123f52f270b0fa7712bac9e052a6
maihuongg <dangmaihuong1108@gmail.com>
2023-05-13 02:33:31 +0700

    chuyển xong fragment appbar

--- app/src/main/java/com/MyAppChat/Activity/MainActivity.java
+++ app/src/main/java/com/MyAppChat/Activity/MainActivity.java
@@ -1,84 +1,59 @@
 package com.MyAppChat.Activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Toast;
 
 import com.example.myappchat.R;
-import com.google.android.material.navigation.NavigationView;
+import com.example.myappchat.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
-    DrawerLayout drawerLayout;
-    Toolbar toolbar;
-    NavigationView navigationView;
 
-    @SuppressLint("MissingInflatedId")
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-        //will hide the title not the title bar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //int flag, int mask
-        setContentView(R.layout.activity_main);
-//        //-------------xu ly drawerLayout
-//        drawerLayout= findViewById(R.id.drawer_layout);
-//        // Open the navigation drawer
-//        drawerLayout.openDrawer(GravityCompat.START);
-//        // Close the navigation drawer
-//        drawerLayout.closeDrawer(GravityCompat.START);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        toolbar = findViewById(R.id.toolbar); //Ignore red line errors
+        replaceFragment(new HomeActivity());
+        binding.bottomNavigationView.setBackground(null);
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.navigation_view);
-        navigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.open_nav,
-                R.string.close_nav);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeActivity()).commit();
-            navigationView.setCheckedItem(R.id.nav_chat);
-        }
-    }
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new HomeActivity());
+                    break;
 
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.nav_chat:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeActivity()).commit();
-                break;
-            case R.id.nav_setting:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingActivity()).commit();
-                break;
+                case R.id.friend:
+                    replaceFragment(new FriendActivity());
+                    break;
 
-            case R.id.nav_logout:
-                Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
-                break;
-        }
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+                case R.id.person:
+                    replaceFragment(new ProfileActivity());
+                    break;
+
+                case R.id.settings:
+                    replaceFragment(new SettingActivity());
+                    break;
+            }
+
+            return true;
+
+        });
 
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
