be80acd6ed32a8178bbb700aecfb9cd7c344a01e
maihuongg <dangmaihuong1108@gmail.com>
2023-05-16 14:24:26 +0700

    update UI

--- app/src/main/java/com/MyAppChat/APIService/ApiService.java
+++ app/src/main/java/com/MyAppChat/APIService/ApiService.java
@@ -9,38 +9,25 @@ import com.MyAppChat.Utils.ListFriendResponse;
 import com.MyAppChat.Utils.LoginResponse;
 import com.MyAppChat.Utils.ProfileResponse;
 import com.MyAppChat.Utils.RegisterRespone;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import org.json.JSONObject;
 
 import java.util.List;
-import java.util.Map;
 
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
 import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
-import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
-import retrofit2.http.Multipart;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
-import retrofit2.http.Part;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface ApiService {
     @POST("user/login")
     @FormUrlEncoded
     Call<LoginResponse> login(@Field("email") String email,
-                        @Field("password") String password);
+                              @Field("password") String password);
 
     @GET("user/profile/{id}")
     Call<ProfileResponse> getProfile(@Path("id") int id);
